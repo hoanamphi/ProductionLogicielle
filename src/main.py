b0f19dcbb5de402ddde9056a27987bbab6d6371d
jonathanchabaud <jonathan.chabaud@etu.univ-nantes.fr>
2018-02-09 15:51:39 +0100

    Work on the database

--- src/main.py
+++ src/main.py
@@ -1,8 +1,17 @@
-import sys
 import csv
 
-with open('data/installations.csv') as csvfile:
-    reader = csv.reader(csvfile)
-    for row in reader:
-        print(row)
-print(sys.version)
+
+def init():
+    with open('data/equipements.csv') as csvfile:
+        equipDict = csv.DictReader(csvfile, delimiter=";")
+    with open('data/equipements_activites.csv') as csvfile:
+        equip_actDict = csv.DictReader(csvfile, delimiter=",")
+    with open('data/installations.csv') as csvfile:
+        installDict = csv.DictReader(csvfile, delimiter=",")
+'''def search(ComInsee, ComLib):
+    init()
+    result = [];
+    for row in equipDict:
+        if(row['ComInsee'] == ComInsee and row['ComLib'] == ComLib):
+            result.append(row);
+    return result'''
