95d5af2982a75922dddd71ce19bea841338810ef
jonathanchabaud <jonathan.chabaud@etu.univ-nantes.fr>
2018-02-09 14:31:49 +0100

    Read files

--- src/main.py
+++ src/main.py
@@ -1,2 +1,8 @@
+import sys
+import csv
 
-print("Hello world");
+with open('data/installations.csv') as csvfile:
+    reader = csv.reader(csvfile)
+    for row in reader:
+        print(row)
+print(sys.version)
