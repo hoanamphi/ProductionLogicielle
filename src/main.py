9e45ebb2706dfab1a8599ed18275552872daddf3
jonathanchabaud <jonathan.chabaud@etu.univ-nantes.fr>
2018-02-13 15:38:13 +0100

    Create a database

--- src/main.py
+++ src/main.py
@@ -1,18 +1,26 @@
 import csv
 import sqlite3
 
-connection = sqlite3.connect("data/test.db")
-
+connection = sqlite3.connect("data/dataBase.db")
+#
 cursor = connection.cursor()
-cursor.execute("CREATE TABLE EQUIPEMENTS (ComInsee INT, Com)")
+
+# cursor.execute("CREATE TABLE EQUIPEMENTS")
+# cursor.execute("CREATE TABLE EQUIPEMENTS_ACTIVITES;")
+# cursor.execute("CREATE TABLE INSTALLATIONS;")
 
 with open('data/equipements.csv') as csvfile:
     equipDict = csv.DictReader(csvfile, delimiter=";")
+    for row in equipDict:
+        first_row = row
+        break
+    head = ",".join(first_row)
+    print(head)
 
 
-with open('data/equipements_activites.csv') as csvfile:
-    equip_actDict = csv.DictReader(csvfile, delimiter=",")
 
-with open('data/installations.csv') as csvfile:
-    installDict = csv.DictReader(csvfile, delimiter=",")
-
+# with open('data/equipements_activites.csv') as csvfile:
+#     equip_actDict = csv.DictReader(csvfile, delimiter=",")
+#
+# with open('data/installations.csv') as csvfile:
+#     installDict = csv.DictReader(csvfile, delimiter=",")
