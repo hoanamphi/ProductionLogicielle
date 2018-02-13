160898ad493dc0afe20bd40d44e3aaee3580023c
jonathanchabaud <jonathan.chabaud@etu.univ-nantes.fr>
2018-02-13 15:10:27 +0100

    Work on the database

--- src/main.py
+++ src/main.py
@@ -1,17 +1,18 @@
 import csv
+import sqlite3
+
+connection = sqlite3.connect("data/test.db")
+
+cursor = connection.cursor()
+cursor.execute("CREATE TABLE EQUIPEMENTS (ComInsee INT, Com)")
+
+with open('data/equipements.csv') as csvfile:
+    equipDict = csv.DictReader(csvfile, delimiter=";")
 
 
-def init():
-    with open('data/equipements.csv') as csvfile:
-        equipDict = csv.DictReader(csvfile, delimiter=";")
-    with open('data/equipements_activites.csv') as csvfile:
-        equip_actDict = csv.DictReader(csvfile, delimiter=",")
-    with open('data/installations.csv') as csvfile:
-        installDict = csv.DictReader(csvfile, delimiter=",")
-'''def search(ComInsee, ComLib):
-    init()
-    result = [];
-    for row in equipDict:
-        if(row['ComInsee'] == ComInsee and row['ComLib'] == ComLib):
-            result.append(row);
-    return result'''
+with open('data/equipements_activites.csv') as csvfile:
+    equip_actDict = csv.DictReader(csvfile, delimiter=",")
+
+with open('data/installations.csv') as csvfile:
+    installDict = csv.DictReader(csvfile, delimiter=",")
+
