3b67a42d6b653315340ea6ea57bdcdd6ffc52abf
CobraDarkPoney <riverleviathan@gmail.com>
2018-04-05 17:41:14 +0200

    Kool webpage and adapt server

--- html/result.tpl
+++
@@ -1,6 +0,0 @@
-<html>
-    <ul>
-        %if INomActivite != None:
-        <p>Nom de l'activité : {{INomActivite}}</p>
-    </ul>
-</html
--- python/server.py
+++ python/server.py
@@ -1,15 +1,19 @@
 # pylint: disable=no-member
 
-from bottle import get, post, run, static_file, request, template
+from bottle import get, post, run, static_file, request, template, route
 
+# CSS routes
+@route('/assets/css/<file>')
+def css(file):
+    return static_file(file, 'html/assets/css/')
+
+# Image routes
+@route('/assets/img/<file>')
+def img(file):
+    return static_file(file, 'html/assets/img/')
 
 @get('/recherche')
 def search_page():
-    return static_file('index.html', 'html/')
-
-@post('/recherche')
-def search():
-    dictio = {'INomActivite': request.forms.get('INomActivite')}
-    return template('html/result', **dictio)
+    return static_file('recherche.html', 'html/')
 
 run(host='localhost', port=8080, debug=True)
